69e2d4f47be9cfcb0b29773f3bc5bed02f6de93a
Eddie Welker <eddie.welker@gmail.com>
2014-08-09 02:54:53 -0400

    added advanced server

--- src/main/scala/gov/nih/nlm/ncbi/server.scala
+++ src/main/scala/gov/nih/nlm/ncbi/server.scala
@@ -1,27 +1,42 @@
 package gov.nih.nlm.ncbi
 
-import com.twitter.finagle.{Http, Service}
-import com.twitter.io.Charsets
+import java.net.InetSocketAddress
+
+import com.twitter.conversions.time._
+import com.twitter.finagle.Service
+import com.twitter.finagle.http.HttpMuxer
 import com.twitter.server.TwitterServer
-import com.twitter.util.{Await, Future}
+import com.twitter.util.{Await, Future, Time}
 import org.jboss.netty.buffer.ChannelBuffers.copiedBuffer
 import org.jboss.netty.handler.codec.http._
 
-object BasicServer extends TwitterServer {
+object AdvancedServer extends TwitterServer {
+
+  val what = flag("what", "hello", "String to return")
+  val addr = flag("bind", new InetSocketAddress(0), "Bind address")
+  val durations = flag("alarms", (1.second, 5.second), "2 alarm durations")
+  val counter = statsReceiver.counter("requests_counter")
+
   val service = new Service[HttpRequest, HttpResponse] {
     def apply(request: HttpRequest) = {
+      log.debug("Received a request at " + Time.now)
+      counter.incr()
       val response =
         new DefaultHttpResponse(request.getProtocolVersion, HttpResponseStatus.OK)
-      response.setContent(copiedBuffer("hello", Charsets.Utf8))
+      val content = copiedBuffer(what() + "\n", Charsets.Utf8)
+      response.setContent(content)
       Future.value(response)
-}
     }
+  }
 
   def main() {
-    val server = Http.serve(":8000", service)
-    onExit {
-      server.close()
+    // We can create a new http server but in that case we profit from the
+    // one already started for /admin/*
+    // The `TwitterServer` trait exposes an `adminHttpServer` that serve all routes
+    // registered in the HttpMuxer object, we just have to add our own.
+    HttpMuxer.addHandler("/echo", service)
+    HttpMuxer.addHandler("/echo/", service)
+    // And wait on the server
+    Await.ready(adminHttpServer)
   }
-  Await.ready(server)
-    }
-  }
+}
